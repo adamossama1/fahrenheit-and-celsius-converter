ac788738ccd3256048d0c852460e4a34d559a591
adamossama1 <adamossama357@gmail.com>
2022-08-18 08:56:15 +0200

    python projects

---
+++ fahrenheit and celsius converter.py
@@ -0,0 +1,23 @@
+print("F° to C° = 1")
+
+print("C° to F° = 2")
+
+x=int(input("1 or 2 ? :"))
+
+if x==1:
+    f=float(input("enter F° :"))
+    result=f-32/1.8
+    print(result)
+    
+
+
+
+
+elif x==2:
+        c=float(input('enter C° :'))
+        result=c*1.8+32
+        print(result)
+
+
+    
+#C° = (F° − 32°) ÷ 1.8
